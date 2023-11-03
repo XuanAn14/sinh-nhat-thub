7fcafc3c1353c724508605ead861d674103f6285
XuanAn14 <131387743+XuanAn14@users.noreply.github.com>
2023-11-03 21:15:12 +0700

    1

---
+++ js/index.js
@@ -0,0 +1,34 @@
+var audio = new Audio('./img/y2mate.com - KHÚC HÁT MỪNG SINH NHẬT  PHAN ĐINH TÙNG  OFFICIAL VIDEO.mp3');
+(function() {
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  var card = $('card'),
+      openB = $('open'),
+      closeB = $('close'),
+      timer = null;
+  
+  console.log('wat', card);
+  openB.addEventListener('click', function () {
+    card.setAttribute('class', 'open-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', 'open-fully');
+      timer = null;
+    }, 1000);
+  });
+  openB.onclick =function(){
+    console.log('heloo')
+    audio.play();
+  };
+  closeB.addEventListener('click', function () {
+    card.setAttribute('class', 'close-half');
+    if (timer) clearTimerout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', '');
+      timer = null;
+    }, 1000);
+  });
+
+}());
